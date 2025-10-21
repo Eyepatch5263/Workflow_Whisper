fba755b2e23d41ca087879ba287719ee616cf5c5
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-10-21 22:37:47 +0530

    ssshhh Nothing is behind the scenes

--- scripts/generate_stego_image.py
+++ scripts/generate_stego_image.py
@@ -2,11 +2,14 @@ import os
 from stegano import lsb
 from PIL import Image
 
-secret = os.environ.get('HIDDEN_SECRET', 'default_secret')
+secret = os.environ.get('HIDDEN_SECRET', '').strip()
+
+if not secret:
+	raise ValueError('Secret is empty. Please set the HIDDEN_SECRET environment variable.')
 
 img = Image.new('RGB', (200, 100), color=(73, 109, 137))
 img.save('original.png')
 
-lsb.hide('OIP.png', secret).save('stego.png')
+lsb.hide('original.png', secret).save('stego.png')
 
-print('Image generated with hidden secret.')
+print('Stego image generated as stego.png with hidden secret.')
