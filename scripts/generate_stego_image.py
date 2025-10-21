ed06e6922fd9e250ba8696c966dc1a36c124faad
Eyepatch5263 <pratyushpragyey@gmail.com>
2025-10-21 22:36:09 +0530

    ssshhh Nothing is behind the scenes

--- scripts/generate_stego_image.py
+++ scripts/generate_stego_image.py
@@ -7,6 +7,6 @@ secret = os.environ.get('HIDDEN_SECRET', 'default_secret')
 img = Image.new('RGB', (200, 100), color=(73, 109, 137))
 img.save('original.png')
 
-lsb.hide('OIP.jpeg', secret).save('stego.png')
+lsb.hide('OIP.png', secret).save('stego.png')
 
 print('Image generated with hidden secret.')
